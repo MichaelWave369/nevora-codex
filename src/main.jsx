68ef337b48913bf51da0d21c0f694ee1420a5a3a
Michael Hughes <microneesia79@gmail.com>
2026-06-02 13:24:17 -0700

    Import public archive mode assets

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,11 @@ import './reviewer.css';
 import './claim-explorer.css';
 import './release-dashboard.css';
 import './reading-mode.css';
+import './archive-mode.css';
 import './reading-mode.js';
 import './claim-explorer.js';
 import './release-dashboard.js';
+import './archive-mode.js';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
