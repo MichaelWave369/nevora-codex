f8b972b22ff77a4921018856ffe2a27ed4c85f84
Michael Hughes <microneesia79@gmail.com>
2026-06-02 13:06:20 -0700

    Import reading mode assets

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import './styles.css';
 import './reviewer.css';
 import './claim-explorer.css';
 import './release-dashboard.css';
+import './reading-mode.css';
+import './reading-mode.js';
 import './claim-explorer.js';
 import './release-dashboard.js';
 
